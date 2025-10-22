36156ef98b7e2507bb3bad611f75697349254fc4
eduardobertozi <webdu15@gmail.com>
2025-10-22 15:39:16 -0300

    feat: adiciona o json-server ao projeto e remove lista fixa de transações em Home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -11,9 +11,5 @@ import { TransactionItem } from './components/transaction-item/transaction-item'
   styleUrl: './home.scss',
 })
 export class Home {
-  transactions = signal<Transaction[]>([
-    // { title: 'Trabalho Freelance', value: 100, type: TransactionType.INCOME },
-    // { title: 'Gasolina', value: 100, type: TransactionType.OUTCOME },
-    // { title: 'Venda na Hotmart', value: 75, type: TransactionType.INCOME },
-  ]);
+  transactions = signal<Transaction[]>([]);
 }
