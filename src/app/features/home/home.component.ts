eab433fdf0f1501955b91274972f51c2fb58f92f
eduardobertozi <webdu15@gmail.com>
2025-10-22 19:46:00 -0300

    feat: adiciona pagina create dentro de /home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Transaction } from '@/app/shared/transaction/interfaces/transaction';
 import { TransactionsService } from '@/app/shared/transaction/services/transactions';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 import { BalanceComponent } from './components/balance/balance.component';
 import { NoTransactionsComponent } from './components/no-transactions/no-transactions.component';
 import { TransactionItemComponent } from './components/transaction-item/transaction-item.component';
@@ -11,6 +13,8 @@ import { TransactionItemComponent } from './components/transaction-item/transact
     BalanceComponent,
     TransactionItemComponent,
     NoTransactionsComponent,
+    MatButtonModule,
+    RouterLink,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -6,4 +6,9 @@ export default [
     loadComponent: () =>
       import('./home.component').then((m) => m.HomeComponent),
   },
+  {
+    path: 'create',
+    loadComponent: () =>
+      import('./pages/create/create.component').then((m) => m.CreateComponent),
+  },
 ] as Routes;
