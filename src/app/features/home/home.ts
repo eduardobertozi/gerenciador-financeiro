adb41d78037cfb04da794e5f7a6faf002233d0c8
eduardobertozi <webdu15@gmail.com>
2025-10-22 16:20:16 -0300

    feat: adiciona busca de transações com uso do TransacionService

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,5 +1,6 @@
 import { Transaction } from '@/app/shared/transaction/interfaces/transaction';
-import { Component, signal } from '@angular/core';
+import { TransactionsService } from '@/app/shared/transaction/services/transactions';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
 import { NoTransactions } from './components/no-transactions/no-transactions';
 import { TransactionItem } from './components/transaction-item/transaction-item';
@@ -10,6 +11,19 @@ import { TransactionItem } from './components/transaction-item/transaction-item'
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+  private transactionService = inject(TransactionsService);
   transactions = signal<Transaction[]>([]);
+
+  ngOnInit(): void {
+    this.getTransactions();
+  }
+
+  private getTransactions() {
+    this.transactionService.getAll().subscribe({
+      next: (transactions) => {
+        this.transactions.set(transactions);
+      },
+    });
+  }
 }
