dea9cdd8b771eb5b2f41955549f94709302ac8ce
eduardobertozi <webdu15@gmail.com>
2025-10-22 20:14:01 -0300

    feat: adiciona formulario para criar transação

--- src/app/features/home/pages/create/create.component.ts
+++ src/app/features/home/pages/create/create.component.ts
@@ -1,11 +1,38 @@
+import { TransactionType } from '@/app/shared/transaction/enums/transaction-type';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: './create.component.html',
-  styleUrl: './create.component.scss'
+  styleUrl: './create.component.scss',
 })
 export class CreateComponent {
+  readonly transactionType = TransactionType;
 
+  form = new FormGroup({
+    type: new FormControl<TransactionType>(TransactionType.OUTCOME),
+    title: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+    value: new FormControl<number>(0, {
+      validators: [Validators.required],
+    }),
+  });
 }
