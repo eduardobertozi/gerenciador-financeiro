94c09f97b9f2da65fd0198d8c3e412433d0399d7
eduardobertozi <webdu15@gmail.com>
2025-10-22 15:13:57 -0300

    feat: adiciona componente para mostrar mensagem quando não há transações

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,19 +1,19 @@
-import { TransactionType } from '@/app/shared/transaction/enums/transaction-type';
 import { Transaction } from '@/app/shared/transaction/interfaces/transaction';
 import { Component, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   transactions = signal<Transaction[]>([
-    { title: 'Trabalho Freelance', value: 100, type: TransactionType.INCOME },
-    { title: 'Gasolina', value: 100, type: TransactionType.OUTCOME },
-    { title: 'Venda na Hotmart', value: 75, type: TransactionType.INCOME },
+    // { title: 'Trabalho Freelance', value: 100, type: TransactionType.INCOME },
+    // { title: 'Gasolina', value: 100, type: TransactionType.OUTCOME },
+    // { title: 'Venda na Hotmart', value: 75, type: TransactionType.INCOME },
   ]);
 }
