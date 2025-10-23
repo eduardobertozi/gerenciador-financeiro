6edcb3dccf60d702f3ae929ac46647e325e0f473
eduardobertozi <webdu15@gmail.com>
2025-10-22 23:29:01 -0300

    feat: adiciona funcionalidade submit ao formulario que cria transação

--- src/app/features/home/pages/create/create.component.ts
+++ src/app/features/home/pages/create/create.component.ts
@@ -1,6 +1,7 @@
 import { TransactionType } from '@/app/shared/transaction/enums/transaction-type';
-import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { TransactionPayload } from '@/app/shared/transaction/interfaces/transaction';
+import { TransactionsService } from '@/app/shared/transaction/services/transactions.service';
+import { Component, inject } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -11,6 +12,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
@@ -22,16 +25,19 @@ import { NgxMaskDirective } from 'ngx-mask';
     MatButtonModule,
     MatButtonToggleModule,
     NgxMaskDirective,
-    JsonPipe,
   ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
 export class CreateComponent {
-  readonly transactionType = TransactionType;
+  private readonly transactionService = inject(TransactionsService);
+  private router = inject(Router);
+  private snackBar = inject(MatSnackBar);
+
+  protected readonly transactionType = TransactionType;
 
   form = new FormGroup({
-    type: new FormControl<TransactionType>(TransactionType.OUTCOME),
+    type: new FormControl<TransactionType>(TransactionType.INCOME),
     title: new FormControl<string>('', {
       validators: [Validators.required],
     }),
@@ -39,4 +45,27 @@ export class CreateComponent {
       validators: [Validators.required],
     }),
   });
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const payload: TransactionPayload = {
+      title: this.form.value.title!,
+      type: this.form.value.type!,
+      value: this.form.value.value!,
+    };
+
+    this.transactionService.post(payload).subscribe({
+      next: () => {
+        this.snackBar.open('Transação criada com sucesso!', 'Fechar', {
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
+        });
+
+        this.router.navigate(['/']);
+      },
+    });
+  }
 }
