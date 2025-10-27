1105360b3a894af341fd87553a67dceaac197d08
eduardobertozi <webdu15@gmail.com>
2025-10-27 13:58:25 -0300

    feat: inclui verificação do estado de logado do usuário no guard com a store

--- src/app/core/auth/guards/is-authenticated-guard.ts
+++ src/app/core/auth/guards/is-authenticated-guard.ts
@@ -1,10 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, RedirectCommand, Router } from '@angular/router';
+import { LoggedInUserStoreService } from '../stores/logged-in-user-store.service';
 
 export const isAuthenticatedGuard: CanActivateFn = () => {
+  const loggedInUserStoreService = inject(LoggedInUserStoreService);
+
+  if (loggedInUserStoreService.isLoggedIn()) {
+    return true;
+  }
+
   const router = inject(Router);
-
   const urlTree = router.parseUrl('auth/login');
-
   return new RedirectCommand(urlTree);
 };
