99f24491c96f1037dff39adb10f7573335677887
eduardobertozi <webdu15@gmail.com>
2025-11-27 12:09:39 -0300

    feat: adiciona diretiva estrutural para mostrar indicador do tipo de transação

--- src/app/features/home/pages/list/components/transaction-item/transaction-item.component.ts
+++ src/app/features/home/pages/list/components/transaction-item/transaction-item.component.ts
@@ -3,15 +3,19 @@ import { Transaction } from '@/app/shared/transaction/interfaces/transaction';
 import { Component, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { TransactionValueComponent } from './components/transaction-value/transaction-value.component';
+import { IsIncomeDirective } from './directives/is-income.directive';
 
 @Component({
   selector: 'app-transaction-item',
   imports: [
     MatCardModule,
     MatButtonModule,
+    MatChipsModule,
     TransactionValueComponent,
     CustomColorDirective,
+    IsIncomeDirective,
   ],
   templateUrl: './transaction-item.component.html',
   styleUrl: './transaction-item.component.scss',
