6dfd28aca58c90b4c7ec9bc58f9bce6598461fb5
eduardobertozi <webdu15@gmail.com>
2026-01-08 14:36:07 -0300

    feat: adiciona sidenav no layout principal

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [
+    HeaderComponent,
+    SidenavItemsComponent,
+    RouterOutlet,
+    MatSidenavModule,
+    MatListModule,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
