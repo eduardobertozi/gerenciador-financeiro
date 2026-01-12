0dac1b83abaf01f9690b5b5a8cfb18d24543a03d
eduardobertozi <webdu15@gmail.com>
2026-01-12 13:24:47 -0300

    feat: implementa model input no componente search

--- src/app/features/transactions/pages/list/list.component.ts
+++ src/app/features/transactions/pages/list/list.component.ts
@@ -2,7 +2,7 @@ import { ConfirmationDialogService } from '@/app/shared/dialog/confirmation/serv
 import { FeedbackService } from '@/app/shared/feedback/services/feedback.service';
 import { Transaction } from '@/app/shared/transaction/interfaces/transaction';
 import { TransactionsService } from '@/app/shared/transaction/services/transactions.service';
-import { Component, inject, input, linkedSignal } from '@angular/core';
+import { Component, inject, input, linkedSignal, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { NoTransactionsComponent } from './components/no-transactions/no-transactions.component';
@@ -26,15 +26,16 @@ import { TransactionsContainerComponent } from './components/transactions-contai
 export class ListComponent {
   private readonly transactionService = inject(TransactionsService);
   private readonly feedbackService = inject(FeedbackService);
-  private readonly confirmationDialogService = inject(
-    ConfirmationDialogService,
-  );
+  private readonly confirmationDialogService = inject(ConfirmationDialogService);
   private readonly activatedRoute = inject(ActivatedRoute);
 
   private readonly router = inject(Router);
 
   transactions = input<Transaction[]>([]);
+
   items = linkedSignal(() => this.transactions());
+
+  searchTerm = signal<string>('');
 
   edit(transaction: Transaction) {
     this.router.navigate(['edit', transaction.id], {
