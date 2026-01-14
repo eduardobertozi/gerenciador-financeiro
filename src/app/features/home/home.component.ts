a89c1ec5a5d7707087624b9cf08093c40d28a8d9
eduardobertozi <webdu15@gmail.com>
2026-01-14 17:30:32 -0300

    feat: adiciona defer block para carregamento do gráfico

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,13 +2,16 @@ import { TransactionType } from '@/app/shared/transaction/enums/transaction-type
 import { sumTransactions } from '@/app/shared/transaction/functions/sum-transactions';
 import { Transaction } from '@/app/shared/transaction/interfaces/transaction';
 import { Component, computed, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { BalanceComponent } from './components/balance/balance.component';
 import { PieChartConfig } from './components/pie-chart/pie-chart-config.interface';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-home',
-  imports: [BalanceComponent, PieChartComponent],
+  imports: [BalanceComponent, PieChartComponent, MatCardModule, MatButtonModule, MatProgressBarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
